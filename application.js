a4600c524a95afd2965ba18088ad1b4c73c44fe9
Junior Sanchez <junior5783@gmail.com>
2022-06-13 23:38:53 -0500

    Adding env.port

--- application.js
+++ application.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
